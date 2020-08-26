57195694d2c679d4c3c9e232824133e23470dd34
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-26 11:22:52 -0300

    added function to generate the dice numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -22,9 +23,17 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   var leftDiceNumber = 5;
+  var rightDiceNumber = 5;
 
   @override
   Widget build(BuildContext context) {
+
+    void changeDiceFaces() {
+        setState(() {
+        leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+    });
+    }
 
     return Center(
       child: Row(
@@ -32,16 +41,17 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = 3;
-                });
+                changeDiceFaces();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                changeDiceFaces();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
